cba3f9649cff691aef572a7134ce67c8529b55c2
medina325 <medina_cdz@hotmail.com>
2023-09-22 09:18:39 -0400

    Criando entidade asset

---
+++ go/internal/market/entity/asset.go
@@ -0,0 +1,20 @@
+package entity
+
+// Asset represents a financial asset.
+//
+// It includes information about the asset's unique identifier (ID), name, and
+// market volume. An asset is a financial instrument or entity that can be
+// traded, such as stocks, bonds, or commodities.
+type Asset struct {
+	ID           string
+	Name         string
+	MarketVolume int
+}
+
+func (a *Asset) NewAsset(id string, name string, marketV int) *Asset {
+	return &Asset{
+		ID:           id,
+		Name:         name,
+		MarketVolume: marketV,
+	}
+}
